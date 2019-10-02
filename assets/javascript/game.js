21fdb90074e056f5314131ee2ca60cfa59337869
RV <rgvegajr@gmail.com>
2019-10-02 17:14:04 -0400

    updated with additional functionality to include displaying wrong guesses

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,19 @@
 // JavaScript source code
 
 //global variables
-let birds = ["hawk", "cardinal", "dove", "osprey", "bluejay", "finch", "bald eagle","heron","egret"];  //array of bird names
+let birds = ["hawk", "cardinal", "dove", "osprey", "bluejay", "finch", "bald eagle", "heron", "egret"];  //array of bird names
 let gameBird = birds[Math.floor(Math.random() * birds.length)]; //name of bird selected randomly from array
-let guessesRem = (gameBird.length + gameBird.length*.75);  //set number of guesses as twice the length of bird name i.e. bird.length*2
+let gameBirdArray = gameBird.split("");
+let guessesRem = (gameBird.length + gameBird.length * .75);  //set number of guesses as twice the length of bird name i.e. bird.length*2
 guessesRem = Math.round(guessesRem);
 let wrongGuesses = []; //array of wrong guesses for displaying
-// let rightGuesses = [];  //array of correct guesses for display.  length set when gameBird set
-let rightGuesses = "";  //array of correct guesses for display.  length set when gameBird set
-
+let rightGuessesArray = [];  //array of correct guesses for display.  length & init elements set when gameBird set
+//let rightGuesses = "";  //array of correct guesses for display.  length set when gameBird set
 let wins = 0;  //number of wins for display
+let badGuesses = "";
+
+let hiddenWord = "";
+
 
 //variables to hold references to html elements for display
 let directionsText = document.getElementById("directions");
@@ -22,74 +26,64 @@ let currentWordText = document.getElementById("currentWord");
 //objects
 let wordGuessGame = {
     init: function () {
-        // wins = guessesRem = wrongGuesses = 0;
-        // gameBird = birds[Math.floor(Math.random() * birds.length)];
-        // guessesAvail = guessesRem = gameBird.length * 2;
-        alert(gameBird);
-        alert(gameBird.length);
-        alert(guessesRem);
-        for (let i=0; i< gameBird.length;i++) {
-            rightGuesses = rightGuesses + "_ ";
-        };
-        document.getElementById("currentWord").innerHTML = rightGuesses;
+        for (let i = 0; i < gameBird.length; i++) {
+            hiddenWord = hiddenWord + " _ ";
+        };            
         document.getElementById("wins").innerHTML = wins;
+        document.getElementById("currentBird").innerHTML = gameBird; //remove after testing
+        document.getElementById("currentWord").innerHTML = hiddenWord;
         document.getElementById("guessesRem").innerHTML = guessesRem;
     },
-    //reset: function () {
-    //    wins, guessesAvail, guessesRem, wrongGuesses = 0;
-    //},
     play: function () {
+        if (guessesRem > 0) {
+            document.onkeyup = function (event) {
+                let guess = event.key;
+                echoText.textContent = guess;
+                //if (gameBirdArray.includes(guess)) {
+                //    echoText.textContent = guess;
+                //    let arrIndex = gameBirdArray.indexOf(guess);
+                //    rightGuessesArray[arrIndex] = guess;
+                //    document.getElementById("currentWord").innerHTML = rightGuessesArray.join("");
 
-        document.onkeyup = function(event) {
-           let guess = event.key;
-           alert(guess);
-        //    echoText.textContent = guess;
-        //     };
-            //check to see if guessed letter is in the gameBird string
-            // var char = gameBird.includes(guess);
-            // alert(char);
-             if (gameBird.includes(guess)) {
-                 alert("correct guess");
-                 echoText.textContent = guess;
+                //} else {
+                //    echoText.textContent = guess;
+                //    wrongGuesses.push(guess);
+                //    document.getElementById("guesses").innerHTML = wrongGuesses.join();
+                //    guessesRem = guessesRem - 1;
+                //    document.getElementById("guessesRem").innerHTML = guessesRem;
+                //};
 
-             } else {
-                 alert("try again");
-                 echoText.textContent = guess;
-             };
+                if (gameBird.includes(guess)) {
+                    for (let i = n = 0; i < gameBird.length; i++) {
+                        if (gameBird.charAt(n) == guess) {
+                            hiddenWord.charAt(n) = guess;
+                            document.getElementById("currentWord").innerHTML = hiddenWord;
+                        } else {
+                            n++;
+                        };
+
+                    };
+                } else {
+                    wrongGuesses.push(guess);
+                    document.getElementById("guesses").innerHTML = wrongGuesses.join();
+                    guessesRem = guessesRem - 1;
+                    document.getElementById("guessesRem").innerHTML = guessesRem;
+
+
+                };
 
 
 
-            //if gameBird.includes(guess) {
-            //    alert("you guessed right!";
-            //} else {
-            //    alert("Try again!");
-            //};
-
-        guessesRem = document.getElementById("guessesRem").innerHTML;
+                if (guessesRem === 0) {
+                    alert("End of Game");
+                    return;
+                }
+            };
         }
-
-        //let guess = document.getElementById("guess");
-        ////guess.value = guess.value.toUpperCase();
-        //let goodGuess = gameBird.includes(guess);
-        //if goodGuess in gameBird {
-        //    guess = wrongGuesses.pop.guess;
-        //    guessesRem = guessesRem - 1;
-        //    };
-
-        
     }
-
-
-
 }
 
-
-
-
-
 //calls
-
-
 wordGuessGame.init();
 //document.getElementById("wins").innerHTML = wins;
 wordGuessGame.play();
